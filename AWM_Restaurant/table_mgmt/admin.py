859411213141656b9e683fdb90e815aba380f405
Spucis <alessio.ruggi000@gmail.com>
2020-07-08 17:55:28 +0200

    Creation of all entities

--- AWM_Restaurant/table_mgmt/admin.py
+++ AWM_Restaurant/table_mgmt/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
-from .models import Person, Client, Table
+from .models import Person, Admin, Client, Client_Auth, Table, Waiter, Order, Menu, Plate
 # Register your models here.
 
 admin.site.register(Person)
+admin.site.register(Admin)
 admin.site.register(Client)
+admin.site.register(Client_Auth)
 admin.site.register(Table)
+admin.site.register(Waiter)
+admin.site.register(Order)
+admin.site.register(Menu)
+admin.site.register(Plate)
--- AWM_Restaurant/table_mgmt/models.py
+++ AWM_Restaurant/table_mgmt/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.forms import PasswordInput
 
 # Create your models here.
 class Table(models.Model):
@@ -6,9 +7,42 @@ class Table(models.Model):
 
 class Person(models.Model):
     code = models.AutoField(primary_key=True)
-    table = models.ForeignKey(Table, on_delete=models.CASCADE)
+
+class Waiter(Person):
+    # Lista ordini con Query
+    name = models.CharField(max_length=50)      # O
+    surname = models.CharField(max_length=50)   # O
+
+class Admin(Person):
+    psw = models.CharField(max_length=32)
+
+class Order(models.Model):
+    code = models.AutoField(primary_key=True)
+    t_code = models.ForeignKey(Table, on_delete=models.CASCADE)
+    waiter = models.ForeignKey(Waiter, on_delete=models.CASCADE)
+    time = models.DateTimeField()
+    plates = {}
 
 class Client(Person):
+    name = models.CharField(max_length=50)      # F
+    surname = models.CharField(max_length=50)   # F
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+
+class Client_Auth(Client):
+    e_mail = models.EmailField(max_length=254)
+
+class Menu(models.Model):
+    day = models.CharField(max_length=20)
+
+class Plate(models.Model):
+    code = models.AutoField(primary_key=True)
     name = models.CharField(max_length=50)
-    surname = models.CharField(max_length=50)
-    email = models.EmailField(max_length=254)
+    days = []
+    description = models.CharField(max_length=100)
+    price = models.FloatField()
+    available = models.BooleanField()
+    menu = models.ForeignKey(Menu, on_delete=models.CASCADE)
+    # Proprietà piatto (vegan etc...)
+
+
+
